3add7127f2b20da7a4396726fca386a5fb0a3bd3
BioKZM <qRnt@yandex.com>
2022-03-01 22:48:52 +0300

    Official Launch

--- cogs/level.py
+++ cogs/level.py
@@ -1,5 +1,6 @@
 import discord
 import time
+import datetime
 from discord.ext import commands
 from functions.userClass import User,levelNames,experiences
 
@@ -16,17 +17,18 @@ class Level(commands.Cog):
             member = ctx.author
 
         user = User(member.id)
+        target = datetime.datetime.fromtimestamp(user.time)
         embed = discord.Embed(title=f"{member.name}#{member.discriminator} adlı kullanıcının bilgileri",description="",color=0x8d42f5)
         embed.add_field(name="Mevcut değerler - 🏆 ",value="Seviyesi = **{}**\n Puanı = **{}**\n Rütbesi = **{}**\n".format(user.level,user.XP,user.levelName,inline=False))
         
         if user.isMaxLevel():
             embed.add_field(name="Bir sonraki rütbe",value=f"**{levelNames[user.level]}** rütbesi için kalan puan = **{(experiences[user.level-1])-user.XP}**" if not user.isMaxLevel() else "Maksimum seviyeye ulaştınız!",inline=False)
-            embed.add_field(name="Günlük Bilgileri",description = f"Haftalık zorunlu gnücellemeye kalan süre = **{user.time-time.time()}**" if not user.time == False else "Günlük bilgisi bulunamadı",inline = False)
+            embed.add_field(name="Günlük Bilgileri",description = f"Haftalık zorunlu gnücellemeye kalan süre = **{target-time.time()}**" if not user.time == False else "Günlük bilgisi bulunamadı",inline = False)
         
         elif not user.isMaxLevel():
             if experiences[user.level] - user.XP <= 0:
                 embed.add_field(name="Bir sonraki rütbe ",value=f"**{levelNames[user.level+1]}** rütbesine ulaştın! Seviye atlamak için ses kanalına girebilirsin.",inline=False)
-                embed.add_field(name="Günlük Bilgileri",description = f"Haftalık zorunlu gnücellemeye kalan süre = **{user.time-time.time()}**" if not user.time == False else "Günlük bilgisi bulunamadı",inline = False)
+                embed.add_field(name="Günlük Bilgileri",description = f"Haftalık zorunlu gnücellemeye kalan süre = **{target-time.time()}**" if not user.time == False else "Günlük bilgisi bulunamadı",inline = False)
                 
             else:
                 if user.level == 0:
